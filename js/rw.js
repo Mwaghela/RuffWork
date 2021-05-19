8746416e77f48333c07bcd1063749cf53235e102
Manish Waghela <manish.b.waghela@gmail.com>
2021-05-19 23:50:53 +0400

    RuffWork Operation added
    1. Add RuffWork created
    2. Delete RuffWork skeleton created
    3. customized css added

--- js/rw.js
+++ js/rw.js
@@ -1,19 +1,20 @@
 var jsonData = {};
 
-function createDynamicCard(title, content){
+function createDynamicCard(component){
     var dynamicContent = "<div class='col-xl-3 col-lg-3 col-md-3'><div class='card bg-light mb-3' style='max-width: 18rem;'>";
-    dynamicContent += "<div class='card-header'>"+title+"</div>";
+    dynamicContent += "<div class='card-header'>"
+    dynamicContent += "<div class='float-right' onclick=\"deleteRuffWork(\'"+component.id+"\')\"><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
+    dynamicContent += "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>";
+    dynamicContent += "<path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>";
+    dynamicContent += "</svg></div>";
+    dynamicContent += "<blockquote class='blockquote text-left'><p class='mb-0'>"+component.title+"</p>";
+    dynamicContent += "<footer class='blockquote-footer text-right'><small>"+component.id+"</small></footer></blockquote>"
+    dynamicContent += "</div>";
     dynamicContent += "<div class='card-body'>";
     //dynamicContent += "<h5 class='card-title'>"+content+"</h5>";
-    dynamicContent += "<p class='card-text'>"+content+"</p>";
+    dynamicContent += "<p class='card-text'>"+component.content+"</p>";
     dynamicContent += "</div></div></div>";
     return dynamicContent;
-}
-
-function addTask(){
-    $('#taskModal').on('shown.bs.modal', function () {
-        $('#taskTitle').trigger('focus');
-    })
 }
 
 async function readFileContent(event) {
@@ -21,15 +22,39 @@ async function readFileContent(event) {
     const text = await file.text();
     jsonData = JSON.parse(text);
     console.log(jsonData);
-    loadContent(jsonData);
+    refreshContent(jsonData);
 }
 
-function loadContent(data) {
+function refreshContent(data) {
     var cardList = data.data.component;
     var mainRow = document.getElementById("mainRow");
     mainRow.innerHTML = "";
     for(var idx=0; idx < cardList.length; idx++) {
-        var dynamicContent = createDynamicCard(cardList[idx].title, cardList[idx].content);
+        var dynamicContent = createDynamicCard(cardList[idx]);
         mainRow.innerHTML += dynamicContent;
     }
+}
+
+function saveRuffWork(){
+    var addTitle = document.getElementById("addTitle").value;
+    var addDetails = document.getElementById("addDetails").value;
+    if(!jsonData.data) {
+        jsonData.data = {};
+    }
+    if(!jsonData.data.component) {
+        jsonData.data.component = [];
+    }
+    var len = jsonData.data.component.length;
+    var date = new Date();
+    var component = {};
+    component.id = "TSK"+date.getTime();
+    component.title = addTitle;
+    component.content = addDetails;
+    jsonData.data.component[len] = component;
+    console.log(jsonData);
+    refreshContent(jsonData);
+}
+
+function deleteRuffWork(id){
+    console.log("Delete Id:"+id);
 }
