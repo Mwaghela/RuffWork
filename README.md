f36636933219fda59bf104ac9554d63e3686ddef
Manish Waghela <manish.b.waghela@gmail.com>
2021-05-21 18:05:42 +0400

    Delete, Search and Download RuffWork funcationality
    1. Delete option added
    2. Search based on title and content added
    3. Download Ruffwork task added
    
    *Readme file update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # RuffWork
+@Start Date: 17 May 2021
+@Author: Manish Waghela
+@Version: 1.0.0
+
+@Objective: 
+Project is used to keep track of RuffWork that we do in daily basis. Current scope of project includes task to be added, deleted, search, sample data upload and downloads. 
+
+@Technical Guide:
+It is stand-alone UI based application used to track Ruff Work that we do as part of Timeboxing or daily activity. Run index.html in browser and start using.
+
+@Future Scope
+1. Alert to be added as part of task so that user will get notification
+2. Creating buckets as ToDo, Inprogress, Done or Block
+3. How to use Tour Guide
--- js/rw.js
+++ js/rw.js
@@ -1,9 +1,10 @@
 var jsonData = {};
+var searchFlag = false;
 
 function createDynamicCard(component){
     var dynamicContent = "<div class='col-xl-3 col-lg-3 col-md-3'><div class='card bg-light mb-3' style='max-width: 18rem;'>";
     dynamicContent += "<div class='card-header'>"
-    dynamicContent += "<div class='float-right' onclick=\"deleteRuffWork(\'"+component.id+"\')\"><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
+    dynamicContent += "<div class='float-right' onclick=\"deleteRuffWork(\'"+component.seqId+"\')\"><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
     dynamicContent += "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>";
     dynamicContent += "<path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>";
     dynamicContent += "</svg></div>";
@@ -48,6 +49,7 @@ function saveRuffWork(){
     var date = new Date();
     var component = {};
     component.id = "TSK"+date.getTime();
+    component.seqId = len;
     component.title = addTitle;
     component.content = addDetails;
     jsonData.data.component[len] = component;
@@ -55,6 +57,36 @@ function saveRuffWork(){
     refreshContent(jsonData);
 }
 
-function deleteRuffWork(id){
-    console.log("Delete Id:"+id);
+function deleteRuffWork(seqId){
+    console.log("Delete seqId:"+seqId);
+    console.log("jsonData:"+JSON.stringify(jsonData));
+    delete jsonData.data.component.splice(seqId, 1);
+    refreshContent(jsonData);
+}
+
+function searchData(value) {
+    if(value == "" && searchFlag) {
+        searchFlag = false;
+        refreshContent(jsonData);
+    } else if(value != "") {
+        searchFlag = true;
+        var searchData = jsonData.data.component.filter(val => val.title.toUpperCase().includes(value.toUpperCase()) || val.content.toUpperCase().includes(value.toUpperCase()));
+        var filterData = {};
+        filterData.data = {};
+        filterData.data.component = searchData;
+        refreshContent(filterData);
+    }
+}
+
+function downloadRuffWork() {
+    console.log("Download");
+    var date = new Date();
+    var fileName = "RuffWork" + date.getTime() + ".json";
+    var content = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonData));
+    var downloadElement = document.createElement('a');
+    downloadElement.setAttribute("href", content);
+    downloadElement.setAttribute("download", fileName);
+    document.body.appendChild(downloadElement);
+    downloadElement.click();
+    downloadElement.remove();
 }
