7e6acc1f80501275ed94946f2b0007f2e505000d
Manish Waghela <manish.b.waghela@gmail.com>
2021-05-22 16:01:09 +0400

    Page Design update - UX
    User Interface changed

--- js/rw.js
+++ js/rw.js
@@ -1,15 +1,17 @@
 var jsonData = {};
+var filterData = {};
 var searchFlag = false;
+var editComponent = {};
+var editFlag = false;
+var editSeqId = 0;
 
 function createDynamicCard(component){
     var dynamicContent = "<div class='col-xl-3 col-lg-3 col-md-3'><div class='card bg-light mb-3' style='max-width: 18rem;'>";
     dynamicContent += "<div class='card-header'>"
-    dynamicContent += "<div class='float-right' onclick=\"deleteRuffWork(\'"+component.seqId+"\')\"><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
-    dynamicContent += "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>";
-    dynamicContent += "<path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>";
-    dynamicContent += "</svg></div>";
+    dynamicContent += "<div class='float-end' onclick=\"deleteRuffWork(\'"+component.seqId+"\')\"><i class='fa fa-trash' style='font-size:18px;' aria-hidden='true'></i></div>";
+    dynamicContent += "<div class='float-end margin-right2px' onclick=\"editRuffWork(\'"+component.seqId+"\')\"><i class='fa fa-pencil' style='margin-right:5px;' aria-hidden='true'></i></div>";
     dynamicContent += "<blockquote class='blockquote text-left'><p class='mb-0'>"+component.title+"</p>";
-    dynamicContent += "<footer class='blockquote-footer text-right'><small>"+component.id+"</small></footer></blockquote>"
+    dynamicContent += "<footer class='blockquote-footer text-end' style='font-size:10px;margin-top:5px;'><small>"+component.id+"</small></footer></blockquote>"
     dynamicContent += "</div>";
     dynamicContent += "<div class='card-body'>";
     //dynamicContent += "<h5 class='card-title'>"+content+"</h5>";
@@ -45,33 +47,61 @@ function saveRuffWork(){
     if(!jsonData.data.component) {
         jsonData.data.component = [];
     }
-    var len = jsonData.data.component.length;
-    var date = new Date();
-    var component = {};
-    component.id = "TSK"+date.getTime();
-    component.seqId = len;
-    component.title = addTitle;
-    component.content = addDetails;
-    jsonData.data.component[len] = component;
-    console.log(jsonData);
-    refreshContent(jsonData);
+
+    if(editFlag) {
+        if(searchFlag) {
+            filterData.data.component[editSeqId].title = addTitle;
+            filterData.data.component[editSeqId].content = addDetails;
+            refreshContent(filterData);
+        } else {
+            jsonData.data.component[editSeqId].title = addTitle;
+            jsonData.data.component[editSeqId].content = addDetails;
+            refreshContent(jsonData);
+        }
+        editComponent = {};
+        editFlag = false;
+        editSeqId = 0;
+    } else {
+        var len = jsonData.data.component.length;
+        var date = new Date();
+        var component = {};
+        component.id = "TSK"+date.getTime();
+        component.seqId = len;
+        component.title = addTitle;
+        component.content = addDetails;
+        jsonData.data.component[len] = component;
+        console.log(jsonData);
+        refreshContent(jsonData);
+    }
+    console.log("FilterData:"+JSON.stringify(filterData));
+    console.log("JsonData:"+JSON.stringify(jsonData));
 }
 
-function deleteRuffWork(seqId){
-    console.log("Delete seqId:"+seqId);
-    console.log("jsonData:"+JSON.stringify(jsonData));
-    delete jsonData.data.component.splice(seqId, 1);
-    refreshContent(jsonData);
+function deleteRuffWork(seqId) {
+    console.log("searchFlag:"+searchFlag);
+    if(searchFlag) {
+        console.log("delete");
+        var id = filterData.data.component[seqId].id;
+        jsonData.data.component = jsonData.data.component.filter(data => data.id != id);
+        delete filterData.data.component.splice(seqId, 1);
+        refreshContent(filterData);
+    } else {
+        console.log("delete");
+        delete jsonData.data.component.splice(seqId, 1);
+        refreshContent(jsonData);
+    }
+    console.log("after delete filterData:"+JSON.stringify(filterData));
+    console.log("after delete jsonData:"+JSON.stringify(jsonData));
 }
 
 function searchData(value) {
     if(value == "" && searchFlag) {
         searchFlag = false;
         refreshContent(jsonData);
+        filterData = {};
     } else if(value != "") {
         searchFlag = true;
         var searchData = jsonData.data.component.filter(val => val.title.toUpperCase().includes(value.toUpperCase()) || val.content.toUpperCase().includes(value.toUpperCase()));
-        var filterData = {};
         filterData.data = {};
         filterData.data.component = searchData;
         refreshContent(filterData);
@@ -89,4 +119,32 @@ function downloadRuffWork() {
     document.body.appendChild(downloadElement);
     downloadElement.click();
     downloadElement.remove();
+}
+
+function showModal() {
+    document.getElementById('addTitle').value = "";
+    document.getElementById('addDetails').value = "";
+    $('#taskModal').modal('show');
+}
+
+function editRuffWork(seqId) {
+    editSeqId = seqId;
+    editFlag = true;
+    if(searchFlag) {
+        editComponent = filterData.data.component[seqId];
+        showModal();
+        document.getElementById('addTitle').value = editComponent.title;
+        document.getElementById('addDetails').value = editComponent.content;
+    } else {
+        editComponent = jsonData.data.component[seqId];
+        showModal();
+        document.getElementById('addTitle').value = editComponent.title;
+        document.getElementById('addDetails').value = editComponent.content;
+    }
+}
+
+function flushData() {
+    editComponent = {};
+    editFlag = false;
+    editSeqId = 0;   
 }
